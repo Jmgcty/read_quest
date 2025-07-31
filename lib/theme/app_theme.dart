a6f38e87c5eccd4a404eaf6c03dc8157f98c7b70
Jmgcty <mendozaraveng@gmail.com>
2025-07-31 11:47:00 +0800

    add theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:read_quest/theme/elevated_button_theme.dart';
 import 'package:read_quest/theme/input_theme.dart';
+import 'package:read_quest/theme/text_theme.dart';
 
 class AppTheme {
   const AppTheme._();
@@ -10,5 +11,6 @@ class AppTheme {
     useMaterial3: true,
     inputDecorationTheme: appInputTheme,
     elevatedButtonTheme: elevatedButtonTheme,
+    textTheme: appTextTheme,
   );
 }
---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final appTextTheme = TextTheme(
+  displayLarge: GoogleFonts.laila(fontSize: 57, fontWeight: FontWeight.bold),
+  displayMedium: GoogleFonts.laila(fontSize: 45, fontWeight: FontWeight.bold),
+  displaySmall: GoogleFonts.laila(fontSize: 36, fontWeight: FontWeight.w600),
+  headlineLarge: GoogleFonts.laila(fontSize: 32, fontWeight: FontWeight.w600),
+  headlineMedium: GoogleFonts.laila(fontSize: 28, fontWeight: FontWeight.w500),
+  headlineSmall: GoogleFonts.laila(fontSize: 24, fontWeight: FontWeight.w500),
+  titleLarge: GoogleFonts.roboto(fontSize: 22, fontWeight: FontWeight.w500),
+  titleMedium: GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500),
+  titleSmall: GoogleFonts.roboto(fontSize: 14, fontWeight: FontWeight.w500),
+  bodyLarge: GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.normal),
+  bodyMedium: GoogleFonts.roboto(fontSize: 14, fontWeight: FontWeight.normal),
+  bodySmall: GoogleFonts.roboto(fontSize: 12, fontWeight: FontWeight.normal),
+  labelLarge: GoogleFonts.roboto(fontSize: 14, fontWeight: FontWeight.w500),
+  labelMedium: GoogleFonts.roboto(fontSize: 12, fontWeight: FontWeight.w500),
+  labelSmall: GoogleFonts.roboto(fontSize: 11, fontWeight: FontWeight.w400),
+);
