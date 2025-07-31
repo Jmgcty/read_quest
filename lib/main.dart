ac49e226a254cafdcfd8e0eac9d4c3c53d127367
Jmgcty <mendozaraveng@gmail.com>
2025-07-31 15:38:59 +0800

    add dep

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:read_quest/firebase_options.dart';
 import 'package:read_quest/router/app_router.dart';
 import 'package:read_quest/theme/app_theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(child: const MyApp()));
 }
 
