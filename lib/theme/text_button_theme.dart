d63c4a7e5badd161d941474b6ab6a2e6d9c0faa5
Jmgcty <mendozaraveng@gmail.com>
2025-07-31 13:09:16 +0800

    add theme

---
+++ lib/theme/text_button_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:read_quest/core/const/app_colors.dart';
+
+final appTextButtonTheme = TextButtonThemeData(
+  style: TextButton.styleFrom(foregroundColor: AppColors.textLabel),
+);
