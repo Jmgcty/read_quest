a3c4a38be673e3e722255de3af1bf48eb8ebb659
Jmgcty <mendozaraveng@gmail.com>
2025-08-05 21:38:22 +0800

    add pref service

---
+++ lib/core/services/pref_storage.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum SharedPrefKeys { sessionId }
+
+class SharedPrefStorage {
+  SharedPrefStorage._();
+  static final SharedPrefStorage instance = SharedPrefStorage._();
+
+  //
+  Future<void> setAuthSession(String sessionId) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString(SharedPrefKeys.sessionId.name, sessionId);
+  }
+
+  Future<String?> getAuthSession() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getString(SharedPrefKeys.sessionId.name);
+  }
+
+  Future<void> removeAuthSession() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.remove(SharedPrefKeys.sessionId.name);
+  }
+}
