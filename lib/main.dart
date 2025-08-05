a51a67d959fc75562a8aa0d060894ac11667b5c9
Jmgcty <mendozaraveng@gmail.com>
2025-08-05 21:38:01 +0800

    remove un necessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:read_quest/firebase_options.dart';
-import 'package:read_quest/router/app_router.dart';
+import 'package:read_quest/features/start/views/get_started_screen.dart';
 import 'package:read_quest/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(child: const MyApp()));
 }
 
@@ -16,13 +13,11 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final routerConfig = ref.watch(routerProvider);
-
-    return MaterialApp.router(
+    return MaterialApp(
       title: 'Flutter Read Quest',
       theme: AppTheme.light,
-      routerConfig: routerConfig,
       debugShowCheckedModeBanner: false,
+      home: GetStartedScreen(),
     );
   }
 }
