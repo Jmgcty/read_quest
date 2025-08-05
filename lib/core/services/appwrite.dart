00e011717335109b3fe7c4f6e12c8545b66557eb
Jmgcty <mendozaraveng@gmail.com>
2025-08-05 23:42:48 +0800

    update file name

--- lib/core/services/appwrite.dart
+++ lib/core/services/appwrite.dart
@@ -1,6 +1,6 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:read_quest/core/const/appwrite_collection_id.dart';
+import 'package:read_quest/core/const/appwrite_id.dart';
 
 final appwriteClientProvider = Provider<Client>((ref) {
   return Client()
