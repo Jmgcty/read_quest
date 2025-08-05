d9eea97c5db16377ed539f7ed518681f1a19bfad
Jmgcty <mendozaraveng@gmail.com>
2025-08-05 21:37:29 +0800

    add asset

--- lib/core/const/app_assets.dart
+++ lib/core/const/app_assets.dart
@@ -1,7 +1,7 @@
 class AppAssets {
   AppAssets._();
   static final String logo = _getImage('logo.png');
-  static final String appBackground = _getImage('app_bg.png');
+  static final String appBackground = _getImage('app_background.png');
   static final String birdCartoon = _getImage('bird_cartoon.png');
   static final String appName = _getImage('app_name.png');
   //
