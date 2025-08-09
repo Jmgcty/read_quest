cace3b7a65277edd1778a5d57d28b584e5ab27b7
Jmgcty <mendozaraveng@gmail.com>
2025-08-09 13:32:10 +0800

    update model

--- lib/core/model/book_model.dart
+++ lib/core/model/book_model.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/widgets.dart';
+import 'dart:io';
+
 import 'package:read_quest/core/model/user_model.dart';
+import 'package:uuid/uuid.dart';
 
 class BookModel {
   final String id;
@@ -8,6 +10,7 @@ class BookModel {
   final String? cover;
   final UserModel? uploader;
   final String file;
+  final String? accepted_at;
 
   BookModel({
     required this.id,
@@ -16,6 +19,7 @@ class BookModel {
     this.cover,
     this.uploader,
     required this.file,
+    this.accepted_at,
   });
 
   factory BookModel.fromJson(Map<String, dynamic> json) {
@@ -28,6 +32,7 @@ class BookModel {
           ? UserModel.fromMap(json['uploader'])
           : null,
       file: json['file'],
+      accepted_at: json['accepted_at'],
     );
   }
 
@@ -38,9 +43,38 @@ class BookModel {
     'cover': cover,
     'uploader': uploader?.uid,
     'file': file,
+    'accepted_at': accepted_at,
   };
 
+  BookModel copyWith({
+    String? id,
+    String? title,
+    String? description,
+    String? cover,
+    UserModel? uploader,
+    String? file,
+    String? accepted_at,
+  }) {
+    return BookModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      cover: cover ?? this.cover,
+      uploader: uploader ?? this.uploader,
+      file: file ?? this.file,
+      accepted_at: accepted_at ?? this.accepted_at,
+    );
+  }
+
   String generateID() {
-    return '${UniqueKey()}_${DateTime.now().millisecondsSinceEpoch}';
+    return Uuid().v4().replaceAll('-', '').substring(0, 8);
+  }
+
+  File getFile() => File(file);
+  String getCurrentPHDateTime() {
+    final now = DateTime.now().toUtc().add(const Duration(hours: 8));
+    final phTime = now.toIso8601String();
+
+    return phTime;
   }
 }
