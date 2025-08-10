8cd01a3b236bc4809516eaf023f3a55ea8918f61
Jmgcty <mendozaraveng@gmail.com>
2025-08-10 16:28:02 +0800

    add initialization

--- lib/features/home/views/home_screen.dart
+++ lib/features/home/views/home_screen.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:read_quest/core/const/app_colors.dart';
 import 'package:read_quest/core/utils/enum/member_enum.dart';
+import 'package:read_quest/features/auth/repository/auth_repository.dart';
 import 'package:read_quest/features/home/views/admin/admin_screen.dart';
 import 'package:read_quest/features/home/views/reader/reader_screen.dart';
 import 'package:read_quest/features/home/views/teacher/teacher_screen.dart';
 import 'package:read_quest/features/membership/provider/get_future_membership.dart';
+import 'package:read_quest/features/membership/repository/member_repository.dart';
+import 'package:read_quest/features/membership/views/membership_pending_screen.dart';
+import 'package:read_quest/features/membership/views/membership_screen.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -16,6 +20,22 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) => init());
+    super.initState();
+  }
+
+  void init() async {
+    final member = await ref.read(memberRepositoryProvider).getMembership();
+    if (member?.status.name == MemberStatus.pending.name) {
+      Navigator.pushReplacement(
+        context,
+        CupertinoPageRoute(builder: (_) => MembershipScreen()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final member = ref.watch(getMembershipProvider);
