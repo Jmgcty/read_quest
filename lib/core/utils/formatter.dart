d09cc35bd8c63b0063af39f6c56787e20060f868
Jmgcty <mendozaraveng@gmail.com>
2025-08-11 11:46:12 +0800

    add formatter

--- lib/core/utils/formatter.dart
+++ lib/core/utils/formatter.dart
@@ -1,3 +1,7 @@
 String formatStringToCapitalFirstLetter(String text) {
   return text[0].toUpperCase() + text.substring(1);
 }
+
+String formatNameToGivenNameOnly(String name) {
+  return name.split(' ')[0];
+}
